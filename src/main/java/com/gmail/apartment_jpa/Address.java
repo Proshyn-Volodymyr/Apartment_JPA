0c2d15efb49ecb83449573069baabd3771b70566
Vladimir Proshin <VlaShin1986@gmail.com>
2023-03-26 21:19:25 +0300

    Address class with builder

--- src/main/java/com/gmail/apartment_jpa/Address.java
+++ src/main/java/com/gmail/apartment_jpa/Address.java
@@ -4,11 +4,17 @@ public class Address {
     private String countryName;
     private String cityName;
     private String streetName;
-    private int buildingNumber;
-    private int apNumber;
-    private int index;
+    private Integer buildingNumber;
+    private Integer apNumber;
+    private Integer index;
 
-    public Address() {
+    public Address(Builder builder) {
+        this.countryName = builder.countryName;
+        this.cityName = builder.cityName;
+        this.streetName = builder.streetName;
+        this.buildingNumber = builder.buildingNumber;
+        this.apNumber = builder.apNumber;
+        this.index = builder.index;
     }
 
     public String getCountryName() {
@@ -57,5 +63,52 @@ public class Address {
 
     public void setIndex(int index) {
         this.index = index;
+    }
+
+    public static class Builder {
+        private String countryName;
+        private String cityName;
+        private String streetName;
+        private Integer buildingNumber;
+        private Integer apNumber;
+        private Integer index;
+
+        public Builder() {
+        }
+
+        public Builder countryName(String countryName) {
+            this.countryName = countryName;
+            return this;
+        }
+
+        public Builder cityName(String cityName) {
+            this.cityName = cityName;
+            return this;
+        }
+
+        public Builder streetName(String streetName) {
+            this.streetName = streetName;
+            return this;
+        }
+
+        public Builder buildingNumber(Integer buildingNumber) {
+            this.buildingNumber = buildingNumber;
+            return this;
+        }
+
+        public Builder apNumber(Integer apNumber) {
+            this.apNumber = apNumber;
+            return this;
+        }
+
+        public Builder index(Integer index) {
+            this.index = index;
+            return this;
+        }
+
+        public Address build() {
+            Address address = new Address(this);
+            return address;
+        }
     }
 }
--- src/main/java/com/gmail/apartment_jpa/App.java
+++ src/main/java/com/gmail/apartment_jpa/App.java
@@ -1,13 +1,45 @@
 package com.gmail.apartment_jpa;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Scanner;
+
+public class App {
+    static EntityManagerFactory emf;
+    static EntityManager em;
+
+    public static void main(String[] args) {
+        emf = Persistence.createEntityManagerFactory("JPA_Apartment");
+        em = emf.createEntityManager();
+        boolean flag = true;
+
+        while (flag) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("1. Show the table");
+            System.out.println("2. Add the new apartment");
+            System.out.println("3. Search apartment by parameter");
+            System.out.println("4. Quit");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    flag = false;
+                    break;
+            }
+        }
+
+
+    }
+
+    private static void addNewApartment() {
+
     }
 }
