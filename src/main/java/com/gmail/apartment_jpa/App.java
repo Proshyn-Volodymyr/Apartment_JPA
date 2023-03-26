9b6d9c9766a60e7f78f5d232310eaaa984bdacad
Vladimir Proshin <VlaShin1986@gmail.com>
2023-03-26 22:20:07 +0300

    addAddress method

--- src/main/java/com/gmail/apartment_jpa/App.java
+++ src/main/java/com/gmail/apartment_jpa/App.java
@@ -3,6 +3,7 @@ package com.gmail.apartment_jpa;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class App {
@@ -41,5 +42,76 @@ public class App {
 
     private static void addNewApartment() {
 
+        boolean flag = true;
+        String district;
+        Address address;
+        Integer square;
+        Integer amountOfRooms;
+        Integer price;
+        while (flag) {
+
+            System.out.println("====== ADDING A NEW APARTMENT ======");
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("1. Enter the name district: ");
+            district = scanner.nextLine();
+
+            System.out.println("2. Enter the address: ");
+
+
+            System.out.println("3. Enter the square of apartment: ");
+            square = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("4. Enter the quantity of rooms");
+            amountOfRooms = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("5. Enter the price of apartment");
+            price = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("1. Submit");
+            System.out.println("0. Back");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+
+                    flag = false;
+                    break;
+                case 0:
+                    flag = false;
+                    break;
+            }
+        }
+    }
+
+    private static Address addAddress() {
+        boolean flag = true;
+        String countryName = "";
+        String cityName = "";
+        String streetName = "";
+        Integer buildingNumber = 0;
+        Integer apNumber = 0;
+        Integer index = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (flag) {
+            System.out.println("====== ADDING A NEW APARTMENT ======");
+            System.out.println("Enter the country: ");
+            countryName = scanner.nextLine();
+            System.out.println("Enter the city: ");
+            cityName = scanner.nextLine();
+            System.out.println("Enter the street name: ");
+            streetName = scanner.nextLine();
+            System.out.println("Enter the building number: ");
+            buildingNumber = scanner.nextInt();
+            System.out.println("Enter the apartment number: ");
+            apNumber = scanner.nextInt();
+            System.out.println("Enter the post index: ");
+            index = scanner.nextInt();
+        }
+        Address address = new Address.Builder().countryName(countryName).cityName(cityName).streetName(streetName)
+                .buildingNumber(buildingNumber).apNumber(apNumber).index(index).build();
+        return address;
     }
 }
